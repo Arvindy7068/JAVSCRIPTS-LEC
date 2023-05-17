017fd6ed0a4893f7c94d3f78e61ace1ea45f2e6c
Arvind Yadav <arvindy7068@gmail.com>
2023-05-17 21:36:19 +0530

    fiesrt lec

--- javascript/interaction.js
+++
@@ -1,7 +0,0 @@
-// let username = prompt("Enter Your Name");
-// console.log(username);
-
-
-
-let isDelete = confirm("Are you sure, you want to delete this user?")
-console.log(isDelete);
--- javascript/main.js
+++
@@ -1,6 +0,0 @@
-console.log('javascript')
-console.log('again printing')
-
-
-alert("hello");
-[1,2].forEach(alert);
